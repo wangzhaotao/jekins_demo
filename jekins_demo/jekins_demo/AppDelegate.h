d4a8be2fdb1863de4e2b9ec4f85dd4a60022aeb5
wangzhaotao <tyler.wang>
2018-12-28 15:57:17 +0800

    first add

---
+++ jekins_demo/jekins_demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jekins_demo
+//
+//  Created by ocean on 2018/12/28.
+//  Copyright © 2018年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jekins_demo/jekins_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jekins_demo
+//
+//  Created by ocean on 2018/12/28.
+//  Copyright © 2018年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
